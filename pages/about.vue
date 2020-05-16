dc47ac0a5cc7b40dc754f6855f32701ef30a3ed2
debs_obrien <debs_obrien@yahoo.co.uk>
2020-05-16 15:13:28 +0200

    modified about text

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <h1 class="title">About Page with SPA fallback</h1>
-        <p class="subtitle">View the source code to see that it wasn't generated</p>
+        <h1 class="title">About Page</h1>
+        <p class="subtitle">This is generated as a SPA fallback - View the source code to see that it wasn't generated</p>
         <NuxtLink to="/">Back to Home</NuxtLink>
     </div>
 </template>
